5b54e5346e66bee1a167e91c8100a270c4508ebb
brooks <brooks.mbaconsultings@gmail.com>
2026-07-30 14:17:17 -0500

    Footer real que se repite en cada pagina (class='footer', motor nativo de wkhtmltopdf), v18.0.1.6.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Formato Cotización Moto Líder',
-    'version': '18.0.1.5.0',
+    'version': '18.0.1.6.0',
     'license': 'LGPL-3',
     'summary': 'Custom Sale Order Report for Moto Líder',
     'description': """
